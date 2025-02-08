96a437b6a6043d13728df2be76aab49aa27dd578
Navdeep Chovatiya <navdeep007.cn@gmail.com>
2025-02-08 23:26:53 +0530

    initial commit

---
+++ src/main/java/com/user/farmtocart/FarmToCartApplication.java
@@ -0,0 +1,13 @@
+package com.user.farmtocart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmToCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmToCartApplication.class, args);
+    }
+
+}
